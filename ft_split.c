03b9ac232efb6c99090c3e6a41ac87a87bd6e0a9
Fahdamine Ksouss <faksouss@e1r7p7.1337.ma>
2022-10-09 14:23:34 +0100

    ft_split is fixed

--- ft_split.c
+++ ft_split.c
@@ -6,88 +6,74 @@
 /*   By: faksouss <faksouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 01:22:35 by faksouss          #+#    #+#             */
-/*   Updated: 2022/10/08 06:15:42 by faksouss         ###   ########.fr       */
+/*   Updated: 2022/10/09 14:21:54 by faksouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-int	ch7al_mn_str(char *s, char c)
+int	is_c(char s, char c)
 {
-	int	lklmat;
-	int	i;
+	if (s == c)
+		return (1);
+	return (0);
+}
 
-	lklmat = 0;
-	i = 0;
-	while (s[i] && s[i] == c)
-		i++;
-	while (s[i])
+int	h_m_str(char *s, char c)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = 0;
+	while (s[++i])
 	{
-		if (s[i] == c && s[i +1] != c)
-			lklmat++;
-		i++;
+		while (s[i] && is_c(s[i], c))
+			i++;
+		if (s[i] && !is_c(s[i], c))
+		{
+			j++;
+			while (s[i] && !is_c(s[i], c))
+				i++;
+		}
 	}
-	return (lklmat);
+	return (j - 1);
 }
 
-unsigned int	ch7al_mn_harf(char *s, char c)
+int	h_m_ch(char *s, char c)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	while ((s[i] && s[i] != c))
+	j = 0;
+	while (s[i] && is_c(s[i], c))
 		i++;
-	return (i);
-}
-
-char	*hz_ht(char *s, char c)
-{
-	int		i;
-	char	*d;
-
-	i = 0;
-	d = (char *)malloc(sizeof(char) * ch7al_mn_harf(s, c));
-	if (!d)
-		return (NULL);
-	while (s[i] != c && s[i])
+	while (s[i] && !is_c(s[i], c))
 	{
-		d[i] = s[i];
 		i++;
+		j++;
 	}
-	d[i] = '\0';
-	return (d);
-}
-
-int	skip_c(char *s, char c)
-{
-	int	i;
-
-	i = 0;
-	if (s[i] == c)
-		while (s[i] && s[i] == c)
-			i++;
-	else
-		while (s[i] && s[i] != c)
-			i++;
-	return (i);
+	return (j);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	int		i;
+	int		st;
 	char	**r;
-	int		n;
 
 	i = -1;
-	n = 0;
-	r = (char **)malloc(sizeof(char *) * (ch7al_mn_str((char *)s, c) + 1));
+	st = 0;
+	r = (char **)malloc(sizeof(char *) * h_m_str((char *)s, c) + 1);
 	if (!r)
 		return (NULL);
-	while (++i < ch7al_mn_str((char *)s, c))
+	while (++i < h_m_str((char *)s, c))
 	{
-		n += skip_c((char *)s + n, c);
-		r[i] = hz_ht((char *)s + n, c);
-		n += skip_c((char *)s + n, c);
+		while (s[st] && is_c(s[st], c))
+			st++;
+		r[i] = ft_substr(s, st, h_m_ch((char *)s + st, c));
+		st += h_m_ch((char *)s + st, c);
 	}
 	r[i] = NULL;
 	return (r);
